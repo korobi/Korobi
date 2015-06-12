a0a234a6c6bf5b74ca168ac80f800568e6753d7e
kashike <kashike@vq.lc>
2015-06-12 13:10:02 -0700

    die in a fire, travis-ci

--- etc/docs.sh
+++ etc/docs.sh
@@ -8,8 +8,8 @@ cd build
 git config --global user.name "Korobi"
 git config --global user.email "korobi@korobi.io"
 git init
-git remote add origin https://${GH_NAME}:${GH_TOKEN}@github.com:korobi/Korobi.git >/dev/null
+git remote add origin https://${GH_NAME}:${GH_TOKEN}@github.com/korobi/Korobi.git >/dev/null
 git checkout --orphan gh-pages
 git add .
-git commit -q -m "Deploy $(date)" >/dev/null
-git push -q -f origin gh-pages >/dev/null
+git commit -q -m "Deploy $(date)" &> /dev/null
+git push -q -f origin gh-pages &> /dev/null
