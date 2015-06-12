f9f79a3a63cbd0eb149141090e10e3de404f7b18
kashike <kashike@vq.lc>
2015-06-12 12:59:10 -0700

    travis-ci

--- etc/docs.sh
+++ etc/docs.sh
@@ -5,10 +5,11 @@ cp -R ./etc/static/. build/
 
 # Deploy
 cd build
+git config --global user.name "Korobi"
+git config --global user.email "korobi@korobi.io"
 git init
-git remote add origin git@github.com:skcoreimaj/KorobiDocs.git
+git remote add origin https://${GH_NAME}:${GH_TOKEN}@github.com:korobi/Korobi.git >/dev/null
 git checkout --orphan gh-pages
 git add .
-git commit -q -m "Deploy $(date)"
-git push -q -f origin gh-pages
-echo "Done! Successfully published docs!"
+git commit -q -m "Deploy $(date)" >/dev/null
+git push -q -f origin gh-pages >/dev/null
